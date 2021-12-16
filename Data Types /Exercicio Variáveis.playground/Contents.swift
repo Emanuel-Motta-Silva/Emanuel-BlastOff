e667b9f7ea057bcee18605ce1d7d53b80b1d3a03
Emanuel-motta <emanuelmotta9@gmail.com>
2021-12-16 18:52:29 -0300

    novo commit

---
+++ Data Types /Exercicio Variáveis.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//Exercicio de Variáveis Implícitas e Explícitas
+
+//Crie Variáveis/Constantes implícitas para os seguintes tipos de dados: String, Int, Double e Bool
+let nome = "Emanuel"
+var idade = 18
+var nota = 5.74
+let verdade = false
+
+//Crie Variáveis/Constantes explícitas para os seguintes tipos de dados: String, Int, Double, Float e Bool
+let sobrenome : String = "motta"
+var idade1 : Int = 48
+let pi : Double = 3.1415925635
+let nota1 : Float = 5.4
+var falso : Bool = true
---
+++ Noções Básicas/Tipo de Dados.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+//String = sequência de caracteres, representado entres aspas ("")
+var nome = "Emanuel"
+
+//Int = Número inteiro, uma variável do tipo inteiro, so aceita inteiro
+var idade = 50
